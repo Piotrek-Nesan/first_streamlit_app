38f079995aaae6f33d78ca85c1b8289fed65fdac
Piotrek-Nesan <62151231+Piotrek-Nesan@users.noreply.github.com>
2022-06-29 14:37:12 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-stremlit.header('Breakfast Menu')
-stremlit.text('Omega 3 & BLueberry Oatmeal')
-stremlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & BLueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
