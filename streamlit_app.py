cd5604b94363264d98dfe958b426fcd0227abe16
Piotrek-Nesan <62151231+Piotrek-Nesan@users.noreply.github.com>
2022-06-29 14:35:53 +0100

    Update streamlit_app.py
    added Header 
    added text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+stremlit.header('Breakfast Menu')
+stremlit.text('Omega 3 & BLueberry Oatmeal')
+stremlit.text('Kale, Spinach & Rocket Smoothie')
